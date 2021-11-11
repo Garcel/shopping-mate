a82a448bff1ab6368797676bb933115a63d8376c
Jose Antonio Garcel <garcel.developer@gmail.com>
2021-11-12 00:07:26 +0100

    simplified and completed proejct files

--- shopping_mate/wsgi.py
+++ shopping_mate/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_mate.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_mate.settings')
 
 application = get_wsgi_application()
