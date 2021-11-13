a346f1cdd0d726ee772f49be6779996cdfd8d57a
Jose Antonio Garcel <garcel.developer@gmail.com>
2021-11-13 02:13:41 +0100

    added custom User model

---
+++ shopping_mate/apps/authentication/managers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_vanilla_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_superuser', False)
+        kwargs.setdefault('is_staff', False)
+
+        if kwargs.get('is_staff') is True:
+            raise ValueError('Vanilla user must not have is_staff=True.')
+
+        if kwargs.get('is_superuser') is True:
+            raise ValueError('Vanilla user must not have is_superuser=True.')
+
+        return self._create_user(email, password, **kwargs)
+
+    def create_staff_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_superuser', False)
+
+        if kwargs.get('is_superuser') is True:
+            raise ValueError('Staff user must not have is_superuser=True.')
+
+        extra_args = kwargs | {'is_staff': True}
+
+        return self._create_user(email, password, **extra_args)
+
+    def create_superuser(self, email, password, **kwargs):
+        extra_args = kwargs | {'is_superuser': True}
+
+        return self._create_user(email, password, **extra_args)
+
+    # helper methods
+    def _create_user(self, email, password, **kwargs):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
--- shopping_mate/settings.py
+++ shopping_mate/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shopping_mate.apps.authentication.apps.AuthenticationConfig',
     'tests',
 ]
 
@@ -125,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
